5c282690302fcfc13cac9dc50bb10f9cdbf3269b
asa129 <asa.jad1290@gmail.com>
2025-07-30 21:18:32 +0900

    db取得API作成

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,4 +1,18 @@
-export async function GET(request: Request) {
-  const url = request.url;
-  return new Response(url);
-}
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
+import { handle } from "hono/vercel";
+import { files } from "@/db/schema";
+
+const app = new Hono().basePath("/api");
+
+app.get("/files", async (c) => {
+  const db = drizzle(
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    (getCloudflareContext().env as any).DB as unknown as D1Database
+  );
+  const filesResponse = await db.select().from(files);
+  return c.json(filesResponse);
+});
+
+export const GET = handle(app);
