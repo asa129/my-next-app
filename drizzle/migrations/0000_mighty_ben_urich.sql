26c8099983074fb080c00029ad91c0f2f7f7fbae
asa129 <asa.jad1290@gmail.com>
2025-07-30 20:15:29 +0900

    Revert "orm設定を追加"
    This reverts commit 60abdd29c8ce530aac63a9d1d89f395570d13ea5.

--- drizzle/migrations/0000_mighty_ben_urich.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `files` (
-	`id` text PRIMARY KEY NOT NULL,
-	`filename` text NOT NULL,
-	`filePath` text NOT NULL,
-	`content` text NOT NULL,
-	`expiresAt` text NOT NULL,
-	`createdAt` text NOT NULL
-);
